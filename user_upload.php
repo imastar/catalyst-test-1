d9063c02ea96068796074be35841cbdc154fa7c0
imastar <imastar@users.noreply.github.com>
2016-02-10 03:45:52 +1100

    Capitalise names
    Change the first name and surname to start with a capital letter.

--- user_upload.php
+++ user_upload.php
@@ -9,7 +9,7 @@
 		)
 	);
 	
-	//Display the inputted command line options (temporary)
+	//(temporary) Display the inputted command line options
 	var_dump($options);
 	
 	//For the CSV file
@@ -18,15 +18,8 @@
 		//should this array reference be in a separate variable?
 	$file = fopen($options["file"],"r");
 	
-	//var_dump(fgetcsv($file));
-	//or could use print_r
-	
-	//empty array to put the csv file into
+	//create an empty array to put the csv file into
 	$lines = [];
-	
-	//skipping the first line of the CSV file because it has the headings name, surname, email
-	//for now
-	fgetcsv($file);
 	
 	//load each line of the CSV file, to the end of the file, into an array
 	while(! feof($file)) {
@@ -36,5 +29,21 @@
 	//Close the CSV file
 	fclose($file);
 	
+	//(temporary) display the contents of the imported data
 	var_dump($lines);
+	
+	//removing the first line of the CSV file because it has the headings name, surname, email
+	array_shift($lines);
+	//removing the last line because it is blank
+	array_pop($lines);
+
+	//capitalise each first name and last name
+	foreach ($lines as &$value) {
+		$value[0] = ucfirst($value[0]);
+		$value[1] = ucfirst($value[1]);
+	}
+	
+	//(temporary) display the contents of the imported data
+	var_dump($lines);
+		
 ?>
