61eca8c300d85898399d3c49d932dba0f4efe3bc
imastar <imastar@users.noreply.github.com>
2016-02-10 00:49:23 +1100

    open csv file
    Opens the CSV file and reads it line-by-line, displaying the contents.

--- user_upload.php
+++ user_upload.php
@@ -11,4 +11,18 @@
 	
 	//Display the inputted command line options (temporary)
 	var_dump($options);
+	
+	//For the CSV file
+	//Open the file given in --file command line option
+		//It should be a CSV file (haven't checked that though)
+		//should this array reference be in a separate variable?
+	$file = fopen($options["file"],"r");
+	
+	//var_dump(fgetcsv($file));
+	//or could use print_r
+	while(! feof($file)) {
+		var_dump(fgetcsv($file));
+	}
+		
+	fclose($file);
 ?>
