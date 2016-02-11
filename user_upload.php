77a5e5454bac7848950bcff3ccc9ece291e31781
imastar <imastar@users.noreply.github.com>
2016-02-11 19:47:31 +1100

    Function csvProcessing
    Putting things into functions. Here it is the csv processing part.

--- user_upload.php
+++ user_upload.php
@@ -20,44 +20,9 @@
 		}
 	}
 	
-	//(temporary) Display the inputted command line options
-	//var_dump($options);
-	
-	//For the CSV file
-	//Open the file given in --file command line option
-		//It should be a CSV file (haven't checked that though)
-		//should this array reference be in a separate variable?
-	$file = fopen($options["file"],"r");
-	
-	//create an empty array to put the csv file into
-	$lines = [];
-	
-	//load each line of the CSV file, to the end of the file, into an array
-	while(! feof($file)) {
-		$lines[] = fgetcsv($file);
-	}
-		
-	//Close the CSV file
-	fclose($file);
-	
-	//(temporary) display the contents of the imported data
-	//var_dump($lines);
-	
-	//removing the first line of the CSV file because it has the headings name, surname, email
-	array_shift($lines);
-	//removing the last line because it is blank
-	array_pop($lines);
+	$filename = $options["file"];
+	$lines = csvProcessing($filename);
 
-	//capitalise each first name and last name
-	foreach ($lines as &$value) {
-		$value[0] = ucfirst($value[0]);
-		$value[1] = ucfirst($value[1]);
-		$value[2] = strtolower($value[2]);
-	}
-	
-	//(temporary) display the contents of the imported data
-	//var_dump($lines);
-	
 	//MySQL
 	
 	//Connect to MySQL using the host, user and password given in the command-line options
@@ -152,4 +117,38 @@
 	
 	//Close the MySQL connection
 	mysqli_close($conn);
+	
+	function csvProcessing($filename) {
+		//For the CSV file
+		//Open the file given in --file command line option
+		//It should be a CSV file (haven't checked that though)
+		//should this array reference be in a separate variable?
+		$file = fopen($filename,"r");
+		
+		//create an empty array to put the csv file into
+		$lines = [];
+	
+		//load each line of the CSV file, to the end of the file, into an array
+		while(! feof($file)) {
+		$lines[] = fgetcsv($file);
+		}
+		
+		//Close the CSV file
+		fclose($file);
+	
+		//removing the first line of the CSV file because it has the headings name, surname, email
+		array_shift($lines);
+		//removing the last line because it is blank
+		array_pop($lines);
+		
+		//capitalise each first name and last name
+		foreach ($lines as &$value) {
+			$value[0] = ucfirst($value[0]);
+			$value[1] = ucfirst($value[1]);
+			$value[2] = strtolower($value[2]);
+		}
+		
+		return $lines;
+	}
+	
 ?>
