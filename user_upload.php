17a6e7e1a31eba347f67c15dc99943eb91544040
imastar <imastar@users.noreply.github.com>
2016-02-11 21:26:44 +1100

    Putting things into functions
    Now there is the main control flow of dry_run or create_table or normal,
    that can be read easily. Everything else is in a function.

--- user_upload.php
+++ user_upload.php
@@ -9,6 +9,7 @@
 		)
 	);
 	
+	//Set the variables for command line options that affect control flow
 	$dry_run = false;
 	$create_table = false;
 	foreach ($options as $key => $value) {
@@ -20,69 +21,58 @@
 		}
 	}
 	
+	//Set the variables for command line options that have parameters
 	$filename = $options["file"];
-	$lines = csvProcessing($filename);
-
-	//MySQL
+	$dbHost = $options["h"];
+	$dbUser = $options["u"];
+	$dbPass = $options["p"];
 	
-	//Connect to MySQL using the host, user and password given in the command-line options
-	$conn = mysqli_connect($options["h"],$options["u"],$options["p"]);
+	//Main control flow
 	
-	if (!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	
-	$filtered = [];
-	//Go through each line to check
-	foreach($lines as &$value) {
-		//Trim leading and trailing whitespace from email so they are not declared invalid due to whitespace
-		$value[2] = trim($value[2]);
-		//Check if the email is valid
-		if(filter_var($value[2], FILTER_VALIDATE_EMAIL))
-		{
-			//Escape any characters that may interfere with the SQL query, eg the apostrophe in surname
-			$value[0] = mysqli_real_escape_string($conn, $value[0]);
-			$value[1] = mysqli_real_escape_string($conn, $value[1]);
-			$value[2] = mysqli_real_escape_string($conn, $value[2]);
-			//Put the data into a new array just for valid entries
-			$filtered[] = $value;
-		}
-		//If the email is invalid, output an error
-		else {
-			error_log("Email invalid: " . $value[2]);
-		}
-	}
-	
+	//If dry run option is selected, we will just process the lines, but not interact with the database
+	//Wasn't really sure of this, what the dry run meant so will just leave it like this
 	if($dry_run) {
-		mysqli_close($conn);
+		//Process the CSV file - loading, capitalization, email validation etc
+		$lines = csvProcessing($filename);
+		//Finish
 		return;
 	}
-	
-	dbCreate($conn);
-	
-	//If the create_table option was specified, we have created the table now
-	//So we can quit
-	if($create_table) {
+	//If create table option is selected, we will connect to the database and create the table, but not interact with the CSV file
+	else if($create_table) {
+		//Connect to the MySQL database
+		$conn = dbConnect($dbHost, $dbUser, $dbPass);
+		//Create table (and database)
+		dbCreate($conn);
+		//Close the MySQL connection
 		mysqli_close($conn);
+		//Finish
 		return;
 	}
-	
-	insertData($filtered, $conn);
-	
-	//Close the MySQL connection
-	mysqli_close($conn);
-	
+	//Otherwise, we will do everything
+	else {
+		//Process the CSV file - loading, capitalization, email validation etc
+		$lines = csvProcessing($filename);
+		//Connect to the MySQL database
+		$conn = dbConnect($dbHost, $dbUser, $dbPass);
+		//Create table (and database)
+		dbCreate($conn);
+		//Insert the CSV data into the table
+		insertData($lines, $conn);
+		//Close the MySQL connection
+		mysqli_close($conn);
+		//Finish
+		return;
+	}
+
+	//Process the CSV file - loading, capitalization, email validation etc
 	function csvProcessing($filename) {
-		//For the CSV file
-		//Open the file given in --file command line option
-		//It should be a CSV file (haven't checked that though)
-		//should this array reference be in a separate variable?
+		//Open the CSV file
 		$file = fopen($filename,"r");
 		
-		//create an empty array to put the csv file into
+		//Create an empty array to put the csv file into
 		$lines = [];
 	
-		//load each line of the CSV file, to the end of the file, into an array
+		//Load each line of the CSV file, to the end of the file, into the array
 		while(! feof($file)) {
 		$lines[] = fgetcsv($file);
 		}
@@ -94,15 +84,43 @@
 		array_shift($lines);
 		//removing the last line because it is blank
 		array_pop($lines);
-		
-		//capitalise each first name and last name
+
+		$filtered = [];
+		//Go through each line
 		foreach ($lines as &$value) {
+			//Capitalise first name and last name
 			$value[0] = ucfirst($value[0]);
 			$value[1] = ucfirst($value[1]);
+			//Email lower case
 			$value[2] = strtolower($value[2]);
-		}
 		
-		return $lines;
+			//Trim leading and trailing whitespace from email so they are not declared invalid due to whitespace
+			$value[2] = trim($value[2]);
+			
+			//Check if the email is valid
+			if(filter_var($value[2], FILTER_VALIDATE_EMAIL))
+			{
+				//Put the data into a new array just for valid entries
+				$filtered[] = $value;
+			}
+			//If the email is invalid, output an error
+			else {
+				error_log("Email invalid: " . $value[2]);
+			}
+		}
+		//Return the processed and filtered array of data
+		return $filtered;
+	}
+	
+	//Connect to the database with the given information, returning a connection
+	function dbConnect($host, $user, $pass) {
+		//Connect
+		$conn = mysqli_connect($host,$user,$pass);
+		//If it didn't work, quit with error
+		if (!$conn) {
+			die("Connection failed: " . mysqli_connect_error());
+		}
+		return $conn;
 	}
 	
 	function dbCreate($conn) {
@@ -123,9 +141,6 @@
 		
 		//If the table already exists, drop it.
 		$sql = "DROP TABLE IF EXISTS users";
-		if(!mysqli_query($conn, $sql)) {
-			echo "Drop if exists failed\n";
-		}
 		
 		//Create the table
 		$sql = "CREATE TABLE users
@@ -134,7 +149,6 @@
 				surname varchar(255),
 				email varchar(255)
 			)";
-		
 		if (mysqli_query($conn, $sql)) {
 			echo "Table created successfully\n";
 		}
@@ -150,10 +164,17 @@
 		}
 	}
 	
+	//Insert the CSV data into the table in the database
 	function insertData($filtered, $conn) {
-		//Go through each line to check and then insert
+		//Go through each line to escape characters and then insert
 		foreach($filtered as &$value)
 		{
+			//Escape any characters that may interfere with the SQL query, eg the apostrophe in surname
+			$value[0] = mysqli_real_escape_string($conn, $value[0]);
+			$value[1] = mysqli_real_escape_string($conn, $value[1]);
+			$value[2] = mysqli_real_escape_string($conn, $value[2]);
+		
+			//Insert the values into the table
 			$sql = "INSERT INTO users (name, surname, email) VALUES ('$value[0]', '$value[1]', '$value[2]')";
 			mysqli_query($conn, $sql);
 		}
